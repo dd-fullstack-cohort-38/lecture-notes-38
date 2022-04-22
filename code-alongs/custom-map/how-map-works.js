464caa997d696f65cfc7ad262d12317ef40365f0
martybonacci <martybonacci@gmail.com>
2022-04-22 08:49:27 -0600

    how map works

---
+++ code-alongs/custom-map/how-map-works.js
@@ -0,0 +1,8 @@
+let myArray = [5, 3, 9, 2]
+
+let myDoubledArray = myArray.map(
+  function (myArrayElement){return myArrayElement * 2}
+  // myArrayElement => myArrayElement * 2
+)
+
+console.log(myDoubledArray)
